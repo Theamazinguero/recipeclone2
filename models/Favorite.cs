20d22226aab7d1508420407f6f10bdf17f50a018
Theamazinguero <114643224+Theamazinguero@users.noreply.github.com>
2025-11-20 14:59:30 -0600

    Create Favorite.cs

---
+++ models/Favorite.cs
@@ -0,0 +1,16 @@
+// File: Models/Favorite.cs
+namespace RecipeApp.Api.Models
+{
+    public class Favorite
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; } = string.Empty;
+        public ApplicationUser? User { get; set; }
+
+        public int RecipeId { get; set; }
+        public Recipe? Recipe { get; set; }
+
+        public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+    }
+}
